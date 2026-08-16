f4fff5bcc99c752e7f5753fdbf226340cc4b5507
Ayush Mark Hembrom <85974485+M4Rk9@users.noreply.github.com>
2026-08-16 10:16:15 +0530

    chore: bootstrap Java 21 Spring Boot application

---
+++ src/test/java/com/traceforge/TraceForgeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.traceforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
